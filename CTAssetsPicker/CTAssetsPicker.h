b18edd056f88936a28ddd20d72f4008163b07586
张 锋 <zhangfeng0408@gmail.com>
2016-03-10 17:37:27 +0800

    use Carthage

---
+++ CTAssetsPicker/CTAssetsPicker.h
@@ -0,0 +1,19 @@
+//
+//  CTAssetsPicker.h
+//  CTAssetsPicker
+//
+//  Created by 张 锋 on 3/8/16.
+//  Copyright © 2016 t.clement.CTAssetsPicker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CTAssetsPicker.
+FOUNDATION_EXPORT double CTAssetsPickerVersionNumber;
+
+//! Project version string for CTAssetsPicker.
+FOUNDATION_EXPORT const unsigned char CTAssetsPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTAssetsPicker/PublicHeader.h>
+
+
