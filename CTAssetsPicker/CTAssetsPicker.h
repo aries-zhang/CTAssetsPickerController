5c69bdca9eb6326b7c78d998263e9ce6446218a4
aries-zhang <feng-aries.zhang@schneider-electric.com>
2016-03-11 22:07:28 +0800

    added all public headers

--- CTAssetsPicker/CTAssetsPicker.h
+++ CTAssetsPicker/CTAssetsPicker.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "CTAssetsPickerController.h"
 
 //! Project version number for CTAssetsPicker.
 FOUNDATION_EXPORT double CTAssetsPickerVersionNumber;
@@ -16,5 +15,43 @@ FOUNDATION_EXPORT double CTAssetsPickerVersionNumber;
 FOUNDATION_EXPORT const unsigned char CTAssetsPickerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CTAssetsPicker/PublicHeader.h>
+#import <CTAssetsPicker/CTAssetsPickerController.h>
+#import <CTAssetsPicker/CTAssetsPageViewController+Internal.h>
+#import <CTAssetsPicker/CTAssetsPickerController+Internal.h>
+#import <CTAssetsPicker/NSBundle+CTAssetsPickerController.h>
+#import <CTAssetsPicker/NSDateFormatter+CTAssetsPickerController.h>
+#import <CTAssetsPicker/NSIndexSet+CTAssetsPickerController.h>
+#import <CTAssetsPicker/NSNumberFormatter+CTAssetsPickerController.h>
+#import <CTAssetsPicker/PHAsset+CTAssetsPickerController.h>
+#import <CTAssetsPicker/PHAssetCollection+CTAssetsPickerController.h>
+#import <CTAssetsPicker/UICollectionView+CTAssetsPickerController.h>
+#import <CTAssetsPicker/UIImage+CTAssetsPickerController.h>
+
+#import <CTAssetsPicker/CTAssetCheckmark.h>
+#import <CTAssetsPicker/CTAssetCollectionViewCell.h>
+#import <CTAssetsPicker/CTAssetCollectionViewController.h>
+#import <CTAssetsPicker/CTAssetItemViewController.h>
+#import <CTAssetsPicker/CTAssetPlayButton.h>
+#import <CTAssetsPicker/CTAssetScrollView.h>
+#import <CTAssetsPicker/CTAssetSelectionButton.h>
+#import <CTAssetsPicker/CTAssetSelectionLabel.h>
+#import <CTAssetsPicker/CTAssetsGridSelectedView.h>
+#import <CTAssetsPicker/CTAssetsGridView.h>
+#import <CTAssetsPicker/CTAssetsGridViewCell.h>
+#import <CTAssetsPicker/CTAssetsGridViewController.h>
+#import <CTAssetsPicker/CTAssetsGridViewFooter.h>
+#import <CTAssetsPicker/CTAssetsGridViewLayout.h>
+#import <CTAssetsPicker/CTAssetsNavigationController.h>
+#import <CTAssetsPicker/CTAssetsPageView.h>
+#import <CTAssetsPicker/CTAssetsPageViewController.h>
+#import <CTAssetsPicker/CTAssetsPickerAccessDeniedView.h>
+#import <CTAssetsPicker/CTAssetsPickerController.h>
+#import <CTAssetsPicker/CTAssetsPickerDefines.h>
+#import <CTAssetsPicker/CTAssetsPickerNoAssetsView.h>
+#import <CTAssetsPicker/CTAssetsViewControllerTransition.h>
+#import <CTAssetsPicker/CTAssetThumbnailOverlay.h>
+#import <CTAssetsPicker/CTAssetThumbnailStacks.h>
+#import <CTAssetsPicker/CTAssetThumbnailView.h>
+
 
 
